b9bdb6d46c311da835f00d26634344f21ea50775
sahhoutiamine <midoriaduko@gmail.com>
2026-01-05 01:49:05 +0100

    add PhotoTag.php model

---
+++ Models/PhotoTag.php
@@ -0,0 +1,27 @@
+<?php
+// PhotoTag.php
+namespace PhotoSphere\Models;
+
+class PhotoTag
+{
+    private int $photoId;
+    private int $tagId;
+    private \DateTime $createdAt;
+    
+    public function __construct(int $photoId, int $tagId)
+    {
+        $this->photoId = $photoId;
+        $this->tagId = $tagId;
+        $this->createdAt = new \DateTime();
+    }
+    
+    // Getters
+    public function getPhotoId(): int { return $this->photoId; }
+    public function getTagId(): int { return $this->tagId; }
+    public function getCreatedAt(): \DateTime { return $this->createdAt; }
+    
+    public static function createAssociation(Photo $photo, Tag $tag): self
+    {
+        return new self($photo->getId(), $tag->getId());
+    }
+}
